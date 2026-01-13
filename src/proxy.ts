5cf6dc4843c9b0a8383da3e71cab5f6557153d91
vistafly <vistafly.services@gmail.com>
2026-01-13 10:30:49 -0800

    Proxy.ts delete

--- src/proxy.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from '@/i18n/routing';
-
-export default createMiddleware(routing);
-
-export const config = {
-  // Exclude API routes from i18n middleware
-  matcher: ['/', '/(en|es)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)',],
-};
